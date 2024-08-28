844cfac10329c79413b48b95d4c9ee6491dc7cdd
Lukas Karuza <lukaskar97@gmail.com>
2024-08-27 20:58:38 -0500

    Continued work on restaurant details.

--- orangecattravelapp.client/src/app/destination-restaurant-details/destination-restaurant-details.component.ts
+++ orangecattravelapp.client/src/app/destination-restaurant-details/destination-restaurant-details.component.ts
@@ -23,6 +23,9 @@ export class DestinationRestaurantDetailsComponent {
   service: string = "4.5" // Default value
   value: string = "4.0" // Default value
   atmosphere: string = "5" // Default value
+  restaurantAbout: string = "In a state where Eating Meat is a religion, there’s only one place to fill up on your favorite fare: Kaminski’s Chop House, voted #1 Best Steakhouse in Wisconsin by EatThis.com in 2021 AND 2022! Treat yourself to the sublime pleasures of hand-cut, dry-aged beef, a variety of seafood delights, well-paired wines, and extraordinary service. Think that isn’t enough? Our view can top it! Request a table by the window and enjoy breathtaking views of the scenic beauty the Wisconsin River provides. Locally owned and operated for over 15 years, the Kaminski Family opened Chop House in 2006 with one goal in mind - offer the best quality product, with even better service. Join us for a dining experience that celebrates the best of the best." // Default value
+  restaurantCuisines: string = "American, Steakhouse, Seafood" // Default value
+  restaurantFeatures: string = "Takeout, Reservations, Private Dining, Seating, Parking Available, Highchairs Available, Wheelchair Accessible, Serves Alcohol, Full Bar, Free Wifi, Accepts Credit Cards, Table Service, Live Music, Gift Cards Available" // Default value
   restaurantImages: string[] = [
     'assets/paris.jpg',
     'assets/tokyo.jpg',

e2f3f254475edd7ee8bb61257f11c98dc7b494f4
Lukas Karuza <lukaskar97@gmail.com>
2024-12-05 21:39:50 -0600

    Started re-working attraction details, will add nearby hotels + restaurants and some basic contact info.

--- orangecattravelapp.client/src/app/destination-attraction-details/destination-attraction-details.component.ts
+++ orangecattravelapp.client/src/app/destination-attraction-details/destination-attraction-details.component.ts
@@ -1,23 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-destination-attraction-details',
   templateUrl: './destination-attraction-details.component.html',
   styleUrls: ['./destination-attraction-details.component.css']
 })
-export class DestinationAttractionDetailsComponent {
+export class DestinationAttractionDetailsComponent implements OnInit {
   attractionName: string = "Noah's Ark Water Park"; // Default name
   businessHoursOpening: string = "10:00 AM"; // Default opening hours
   businessHoursClosing: string = "5:00 PM"; // Default closing hours
   attractionWebsite: string = "https://www.noahsarkwaterpark.com/" // Default website
   attractionAddress: string = "1410 Wisconsin Dells Pkwy, Wisconsin Dells, WI 53965-8445"; // Default address
+  encodedAddress: string = ""
+  attractionRating: number = 4.0 // Default value
+  attractionReviewNumber: number = 152 // Default value
   attractionImages: string[] = [
     'assets/paris.jpg',
     'assets/tokyo.jpg',
     'assets/newyork.jpg'
   ];
-  constructor() { }
+  constructor() {
+    this.encodedAddress = encodeURIComponent(this.attractionAddress);
+  }
 
   ngOnInit(): void {
+
   }
 }

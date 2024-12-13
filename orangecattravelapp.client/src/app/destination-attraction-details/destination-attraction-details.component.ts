e0bc5bba19582cd615e0a9119f893a0d8f088a50
Lukas Karuza <lukaskar97@gmail.com>
2024-12-12 21:05:21 -0600

    Corrected overview view and continued work on attraction details view.

--- orangecattravelapp.client/src/app/destination-attraction-details/destination-attraction-details.component.ts
+++ orangecattravelapp.client/src/app/destination-attraction-details/destination-attraction-details.component.ts
@@ -7,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DestinationAttractionDetailsComponent implements OnInit {
   attractionName: string = "Noah's Ark Water Park"; // Default name
+  attractionSubCategory: string = "Water & Amusement Park"; // Default sub category
   businessHoursOpening: string = "10:00 AM"; // Default opening hours
   businessHoursClosing: string = "5:00 PM"; // Default closing hours
   attractionWebsite: string = "https://www.noahsarkwaterpark.com/" // Default website
   attractionAddress: string = "1410 Wisconsin Dells Pkwy, Wisconsin Dells, WI 53965-8445"; // Default address
+  attractionPhone: string = "+1 800-222-6624"; // Default phone number
   encodedAddress: string = ""
   attractionRating: number = 4.0 // Default value
   attractionReviewNumber: number = 152 // Default value
